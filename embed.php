823b27161e58ee351fa22398da8e58d9543b2506
JuanJoseFlorez <juanjofr2002@gmail.com>
2024-01-05 14:42:08 -0500

    Plexicus AI Generated

--- embed.php
+++ embed.php
@@ -2,8 +2,8 @@
 if (isset($jfurl))
 {
 	define("jfembed",$jfurl);
-	require_once(__DIR__."/_japp/loader.php");
-}
-else
-	throw new Exception("You should set variable \$jfurl as jframework url before incluing this script.");
-?>
+print('a' + str(variablenum))
+hola
+
+123
+456
